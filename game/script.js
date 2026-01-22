7f867451a00338b979a95616d42f7ef40c194f33
Ivannnn01 <ivan.j.irawan@gmail.com>
2026-01-22 08:24:17 +0800

    Update script.js

--- game/script.js
+++ game/script.js
@@ -22,7 +22,7 @@ window.addEventListener("keydown", (event) => {
 // --- HORSE SPAWNING FACTORY ---
 function spawnHorse() {
     const horse = document.createElement("img");
-    horse.src = "./image/horse_icon.png"; // Must match your GitHub filename exactly
+    horse.src = "image/horseicon.png"; // Must match your GitHub filename exactly
     horse.className = "falling-horse";
     
     // Randomize starting X position
